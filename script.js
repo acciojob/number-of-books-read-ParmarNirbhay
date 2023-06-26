655e7cb5951d73f2fefce3852b5653fb1154fe77
ParmarNirbhay <119995697+ParmarNirbhay@users.noreply.github.com>
2023-06-26 15:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    const library = [
+      {
+        author: "Bill Gates",
+        title: "The Road Ahead",
+        readingStatus: true,
+      },
+      {
+        author: "Steve Jobs",
+        title: "Walter Isaacson",
+        readingStatus: true,
+      },
+      {
+        author: "Suzanne Collins",
+        title: "Mockingjay: The Final Book of The Hunger Games",
+        readingStatus: false,
+      },
+    ];
+     
+    const numberOfBooksRead = () => {
+      // write your code here
+    	    // write your code here
+        let count=0;
+          library.forEach((i)=>{
+           if(i.readingStatus===true)
+           {
+             count++;
+           }
+          })
+          return count;
+     
+    };
+     
+    // Do not change the code below
+     
+    alert(numberOfBooksRead());
